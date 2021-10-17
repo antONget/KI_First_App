033482e8e62551c59dc8fc5c7465a96be3647f66
antONget Anton <a.l.ponomarev@mail.ru>
2021-10-17 18:35:33 +0400

    создан проект функции предикта изображения и вывода информации в лейбл

--- First/main.py
+++ First/main.py
@@ -1,7 +1,8 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from plyer import filechooser
-#model = load_model('First/best_model+815.h5')
+import numpy as np
+from tensorflow.keras.models import  load_model # из кераса подгружаем  метод загрузки предобученной модели
 KV = """
 ScreenManager:
     MDScreen:
@@ -54,6 +55,35 @@ class ImageClassify(MDApp):
             self.root.ids.img.source = selection[0]
             print(selection[0])
 
+    def recognition(image):
+        model = load_model('First/best_model+815.h5')
+        # наименование класса попорядку
+        numClass = ['DJI_Inspire_2',
+                    'DJI_Matrice_210-RTK',
+                    'DJI_Matrice_600_Pro',
+                    'DJI_Mavic_Moscow',
+                    'DJI_Mavic_Pro_Platinum',
+                    'DJI_Phantom_4',
+                    'DJI_Phantom_4_Pro_Plus',
+                    'DJI_Spark',
+                    'Moscow_Noise']
+
+        img_width = 227  # Ширина изображения
+        img_height = 227  # Высота изображения
+
+        #img_path = '2450MHz_12.5MSps_17.05.2021_11-24-03_duration=37.1917ms_feature_type=fft_spectrogram_6.png'  # путь к отдельному экземпляру данных
+        #img = image.load_img(filename, target_size=(img_height, img_width))  # загружаем фото в переменную
+        #img = Image.crop(0,0,img_height,img_width)
+        img = np.array(image)  # переводим в массив
+        img = img / 255.0  # нормализуем
+        img = np.expand_dims(img, axis=0)  # добавляем дополнительную размерность, так как НС просит размер батч сайза
+        listProbablyImg = model.predict(img)
+
+        predClass = np.argmax(listProbablyImg)
+        percentClass = round(listProbablyImg[0,predClass]*100,2)
+        nameClass = numClass[predClass]
+        # выводим информацию по распознованию в лейбл
+        text_elem.update("This image belongs to {} class with probability {} %".format(predClass, listProbablyImg))
 
 if __name__ == "__main__":
     ImageClassify().run()
